4036eb28d3b2a7b30ee97def1811089bf8959056
emine <emineispir.ce@gmail.com>
2021-11-30 20:14:23 +0300

    User controller added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::apiResource('users', UserController::class);
