2cec12b2293721accf1bbbf10780c1fff692ac61
emine <emineispir.ce@gmail.com>
2021-11-30 20:12:48 +0300

    User table and seeders added

---
+++ database/migrations/2021_11_30_170330_add_contact_columns_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddContactColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('surname')->nullable();
+            $table->string('phone_number');
+            $table->text('address')->nullable();
+            $table->dropColumn('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('surname');
+            $table->dropColumn('phone_number');
+            $table->dropColumn('address');
+            $table->addColumn('password');
+        });
+    }
+}
